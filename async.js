48060e4a6ff29129f90ee4ce2fbf614951f537a9
Mukarusine Lilian <mukarusinelilian@gmail.com>
2023-04-11 02:25:42 +0200

    javascript promises

---
+++ async.js
@@ -0,0 +1,76 @@
+
+let stocks = {
+fruits : ["Strawberry", " grapes", "banana", " apple"],
+liquid : ["water", "ice"],
+holder : ["cone", "cup", "stick"],
+toppings : ["chocolate","peanuts"]
+};
+
+let  is_shop_open = true;
+
+
+let order = (time, work) => {
+
+  return new Promise((resolve, reject) => {
+   if (is_shop_open){
+
+    setTimeout(()=>{
+
+      resolve( work() )
+
+    },time )
+   
+
+   } else {
+    reject(console.log("our shop is closed"))
+
+   }
+
+  })
+}
+
+order(2000, () =>console.log(`${stocks.fruits[0]} was selected`))
+
+.then(() =>{
+  return order(0000, () => console.log("production has started"))
+})
+
+
+.then(() =>{
+  return order(2000, () => console.log("the food was chopped"))
+})
+
+
+.then(() =>{
+  return order(1000, () => {
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was selected`)
+  })
+})
+
+
+.then(() => {
+return order(1000, () => console.log("start the machine"))
+
+})
+
+
+.then(() =>{
+  return order(2000, () =>  {
+    console.log(`ice cream placed on ${stocks.holder[0]}`)
+  } )
+})
+
+
+
+.then(() =>{
+  return order(3000, () =>{
+    console.log(`${stocks.toppings[0]} was selected`)
+  })
+})
+
+
+
+.then(() => {
+  return order(2000, () => console.log("ice cream was served"))
+})
+
